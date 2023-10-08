a360c7411d7fcc84202556f2a46c27b6aa85296e
Ashkie <ashkiecharles@gmail.com>
2023-10-08 17:01:48 -0600

    active nav bar indicator

--- main.js
+++ main.js
@@ -42,3 +42,9 @@ let toggleNav = function() {
 
 
 }
+
+document.querySelectorAll('.nav-link').forEach(link => {
+  if(link.href === window.location.href){
+    link.setAttribute('aria-current', 'page')
+  }
+})
