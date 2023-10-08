a9302b132fa960c94fbe3ef34d6ee5a1a3c6bae1
Ashkie <ashkiecharles@gmail.com>
2023-10-08 11:27:34 -0600

    menu bar is done

---
+++ main.js
@@ -0,0 +1,44 @@
+/*Sidebarmenu*/
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+  let getSideBar = document.querySelector(".nav-sidebar");
+  let getSideBarUl = document.querySelector(".nav-sidebar ul");
+  let getSideBarTitle = document.querySelector(".nav-sidebar span");
+  /* Gets the link nav */
+  let getSideBarUla = document.querySelectorAll(".nav-sidebar a");
+
+
+
+/* This function is the reason why if the user clicks on the menu icon it opens and closes */
+  if(toggleNavStatus == false) {
+    getSideBarUl.style.visibility = "visible";
+    getSideBar.style.width = "272px";
+    getSideBarTitle.style.opacity = "0.5";
+
+    let arrayLength = getSideBarUla.length;
+    for (let i = 0; i < arrayLength; i++){
+      getSideBarUla[i].style.opacity = "1"
+    }    
+
+    toggleNavStatus = true;
+  }
+
+  else if (toggleNavStatus == true) {
+    
+    getSideBar.style.width = "60px";
+    getSideBarTitle.style.opacity = "0.5";
+
+    let arrayLength = getSideBarUla.length;
+    for (let i = 0; i < arrayLength; i++){
+      getSideBarUla[i].style.opacity = "0"
+    }    
+    
+    getSideBarUl.style.visibility = "hidden";
+
+    toggleNavStatus = false;
+  }
+
+
+
+}
